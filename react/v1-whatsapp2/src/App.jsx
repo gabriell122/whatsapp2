f8f33cd8042cc041a8c131b167839bb3738ac068
gabriell122 <gabriell.haines@etec.sp.gov.br>
2025-08-19 17:11:39 -0300

    up

---
+++ react/v1-whatsapp2/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="c12 h1h df ac jcc green">
+        <div className="sidbar c2 cm3 cl4 h1 df ac jcc cinza pa l0 t0">
+
+        </div>
+        <div className="body c10 cm9 cl8 h1 df ac jcsa pa r0 t0 fdc">
+            <div className="c10 cl11 h075 br10 black">
+
+            </div>
+            <div className="mensage h40 c10 cl11 br10 black">
+                <input type="text" className='c11 cl10 h1 brl10 bsbb'/>
+                <input type="button" value="Enviar" className='c1 cl2 h1 bsbb brr10'/>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
